334e438338c1f131126afea8f6a3e8b1e19a864e
NilayMancini <mancininilay@gmail.com>
2023-12-22 12:19:41 +0100

    o

--- src/pgen/dyngr_tov_dipole.cpp
+++ src/pgen/dyngr_tov_dipole.cpp
@@ -211,9 +211,8 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
 
     // FIXME: assumes ideal gas!
     // Set hydrodynamic quantities
-    w0_(m,IDN,k,j,i) = fmax(rho, tov_.dfloor);
-    //w0_(m,IEN,k,j,i) = fmax(p, tov_.pfloor)/(tov_.gamma - 1.0);
-    w0_(m,IPR,k,j,i) = fmax(p, tov_.pfloor);
+    w0_(m,IDN,k,j,i) = fmax(rho, tov.dfloor);
+    w0_(m,IPR,k,j,i) = fmax(p, tov.pfloor);
     w0_(m,IVX,k,j,i) = vr*x1v/r;
     w0_(m,IVY,k,j,i) = vr*x2v/r;
     w0_(m,IVZ,k,j,i) = vr*x3v/r;
