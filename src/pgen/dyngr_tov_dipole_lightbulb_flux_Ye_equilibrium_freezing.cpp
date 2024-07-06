fb9b26fe81ae8a1803dae0024ca20d53d657c5c3
NilayMancini <mancininilay@gmail.com>
2024-07-06 18:22:03 +0200

    o

--- src/pgen/dyngr_tov_dipole_lightbulb_flux_Ye_equilibrium_freezing.cpp
+++ src/pgen/dyngr_tov_dipole_lightbulb_flux_Ye_equilibrium_freezing.cpp
@@ -57,7 +57,7 @@ struct tov_pgen {
 Real C;
 Real B;
 Real rho_cut;
-Real rho_cut2;
+Real r_cut2;
 Real r_cut;
 Real T;
 Real Kappatilde;
@@ -154,7 +154,7 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
   C = pin->GetOrAddReal("problem", "C", 0.0);
   B = pin->GetOrAddReal("problem", "B", 0.0);
   rho_cut = pin->GetOrAddReal("problem", "rho_cut", 1.0);
-  rho_cut2 = pin->GetOrAddReal("problem", "rho_cut2", 1.0);
+  r_cut2 = pin->GetOrAddReal("problem", "r_cut2", 1.0);
   r_cut = pin->GetOrAddReal("problem", "r_cut", 1.0);
   T = pin->GetOrAddReal("problem", "T", 0.0); 
   Kappa = pin->GetReal("problem", "kappa");
@@ -832,7 +832,7 @@ void neutrinolightbulb(Mesh* pm, const Real bdt){
   Real Q = C; //Lve,52
   Real Tnu = T; //Tnue
   Real rhocut = rho_cut;
-  Real rhocut2 = rho_cut2;
+  Real rcut2 = r_cut2;
   Real rcut = r_cut;
   Real r0_ = r0;
   Real b_norm_ = b_norm;
@@ -1049,9 +1049,17 @@ void neutrinolightbulb(Mesh* pm, const Real bdt){
       z = 1.0;
     }
 
+    /*
     Real z2;
     if (w0(m,IDN,k,j,i)>rhocut2){
-      z2 = exp(3.0*(1.0 - w0(m,IDN,k,j,i)/rhocut2));
+      z2 = exp(2.0*(1.0 - w0(m,IDN,k,j,i)/rhocut2));
+    } else {
+      z2 = 1.0;
+    }*/
+
+   Real z2;
+   if (r<rcut2){
+      z2 = exp(10*(-1.0 + r/rcut2));
     } else {
       z2 = 1.0;
     }
