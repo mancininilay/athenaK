28f6c4d1c6cf5dfef522bc26bab59ea53e44b887
NilayMancini <mancininilay@gmail.com>
2024-09-27 16:52:18 -0400

    o

--- vis/python/plot_hst_final.py
+++ vis/python/plot_hst_final.py
@@ -50,7 +50,7 @@ def main(**kwargs):
         plt.figure(figsize=(10, 6))
         plt.axvline(x=xb, color='black' ,linestyle='-',linewidth=0.4)
         plt.axvspan(0, xb, alpha=0.21, color='red', label= 'Transient Phase')
-        plt.axvspan(xb,x_vals[-1], alpha=0.2, color='green', label= 'Steady State')
+        plt.axvspan(xb,x_vals[-1], alpha=0.2, color='blue', label= 'Steady State')
         plt.ylabel(r'$\dot{M}(t)$ [$M_{\odot}$ s$^{-1}$]')
         plt.xlabel(r'$t$ [ms]')
         plt.plot(x_vals, y_vals,color='black',linewidth=1.5)
